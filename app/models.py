db3e4dca6388c7daaccf5875c2ebba5ffefca61a
Karl Hobley <karlhobley10@gmail.com>
2016-01-04 12:46:44 +0000

    Created MyModel

--- app/models.py
+++ app/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class MyModel(models.Model):
+    my_field = models.CharField(max_length=255, db_index=True)
